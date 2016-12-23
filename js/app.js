d6baa0860593cde74746a4aec325801952a329bc
Mauya <smkadungure@live.co.uk>
2016-12-23 21:36:37 +0000

    initial commit after merge

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('bandApp', ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.bootstrap']);
+
+app.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+    })    
+    .when('/booking', {
+        templateUrl: 'templates/booking.html',
+        controller: 'BookingController'
+    })
+    .when('/contact', {
+        templateUrl: 'templates/contact.html',
+        controller: 'ContactController'
+    })
+    .when('/gallery', {
+        templateUrl: 'templates/gallery.html',
+        controller: 'GalleryController'
+    })
+    .when('/music', {
+        templateUrl: 'templates/music.html',
+        controller: 'MusicController'
+    })
+    .when('/videos', {
+        templateUrl: 'templates/video.html',
+        controller: 'VideoController'
+    })
+    .when('/photos', {
+        templateUrl: 'templates/photo.html',
+        controller: 'PhotoController'
+    })    
+    .otherwise({
+        redirectTo: '/'
+    })
+    $locationProvider.html5Mode(true);    
+}]);
+
---
+++ js/controllers/AboutController.js
@@ -0,0 +1,34 @@
+app.controller('AboutController', ['$scope', function($scope){
+    $scope.memberTitle= "The Band Members and Achivements";
+    $scope.members=[
+    {
+      "name":"member one",
+      "img":"images/member1.jpg",
+      "details":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+    },
+
+    {
+      "name":"member Two",
+      "img":"images/member2.jpg",
+      "details":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+    },
+
+    {
+      "name":"member Three",
+      "img":"images/member3.jpg",
+      "details":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+    },
+    
+    {
+      "name":"member Four",
+      "img":"images/member4.jpg",
+      "details":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+    },
+
+    {
+      "name":"member Five",
+      "img":"images/member5.jpg",
+      "details":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+    }  
+    ];
+}]);
