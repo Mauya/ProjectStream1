92bb6c519aa53dc5a446baa31ef766f955006e0c
Mauya <smkadungure@live.co.uk>
2016-12-24 09:27:28 +0000

    changes to about controller

--- js/controllers/AboutController.js
+++ js/controllers/AboutController.js
@@ -2,40 +2,33 @@ app.controller('AboutController', ['$scope', function($scope){
     $scope.memberTitle= "The Band Members and Achivements";
     $scope.members=[
     {
-      name:"member one",
-      image:"images/member1.jpg",
-      description:"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+      "name":"member one",
+      "image":"images/member1.jpg",
+      "description":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
     },
 
     {
-      name:"member Two",
-      image:"images/member2.jpg",
-      description:"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+      "name":"member Two",
+      "image":"images/member2.jpg",
+      "description":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
     },
 
     {
-      name:"member Three",
-      image:"images/member3.jpg",
-      description:"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+      "name":"member Three",
+      "image":"images/member3.jpg",
+      "description":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
     },
     
     {
-      name:"member Four",
-      image:"images/member4.jpg",
-      description:"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+      "name":"member Four",
+      "image":"images/member4.jpg",
+      "description":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
     },
 
     {
-      name:"member Five",
-      image:"images/member5.jpg",
-      description:"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
+      "name":"member Five",
+      "image":"images/member5.jpg",
+      "description":"Duo Reges: constructio interrete. Odium autem et invidiam facile vitabis. Et quidem, inquit, vehementer errat; Quid enim est a Chrysippo praetermissum in Stoicis? Unum est sine dolore esse, alterum cum voluptate."
     }  
     ];
 
-    $scope.currentImage = _.first($scope.images);  
-     
-    $scope.setCurrentImage = function(image) {
-
-    $scope.currentImage = image;
-  };
-}]);
