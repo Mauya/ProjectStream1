bd0b4002375fedd49572b5338e9d68f87ead9bab
Mauya <smkadungure@live.co.uk>
2016-12-27 00:15:57 +0000

    Changes to about images

--- js/controllers/ContactController.js
+++ js/controllers/ContactController.js
@@ -2,7 +2,7 @@ angular.module('RouteControllers', [])
 app.controller("ContactController", function($scope){
   var contact = {
     address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
-    email:"symphonycrew@sc.ie",
+    email:"swingcrew@sc.ie",
     telephone: "xx xxx xxxx"
   };
   $scope.contact=contact;
