485410f436036fdeae789e8db153561248147af5
Mauya <smkadungure@live.co.uk>
2016-12-28 19:40:32 +0000

    add music html and derective and audio service

---
+++ js/directive.js
@@ -0,0 +1,10 @@
+angular.module('bandApp').directive('audioPlayer', function() {
+  return {
+        restrict: "E",
+        scope: {},
+        templateUrl: "./player.html",
+        link: function (scope, element, attrs) {
+
+      }
+    };
+});
---
+++ js/service.js
@@ -0,0 +1,40 @@
+//service enable our palyer to actually play. service return and object.
+myApp.service("audioService", function (audiofactory) {
+	var audioPlayer = {
+		isPaused: true,
+		//few sample tracks, there are to be replaced by band music files.
+		audioList: [{
+			Audio:"music/26886.mp3",
+			id: 0
+		},
+		{
+			Audio:"music/minion-xmas.make4fun.com.24eb4.mp3"
+		}		
+		],
+		//set source of the audio player to the current track.
+	currentIndex: 0,
+	     
+	    play: function () {
+	       myPlayer.isPaused = false;
+	       AudioFactory.src = myPlayer.trackList[myPlayer.currentIndex];
+	       AudioFactory.play();
+	    },
+	     
+	    pause: function () {
+	        myPlayer.isPaused = !myPlayer.isPaused;
+	        if (myPlayer.isPaused) {
+	            AudioFactory.pause();
+	        } else {
+	            AudioFactory.play();
+	        }
+	    },
+	  };
+	   
+	  return myPlayer;
+	});
+
+angular.module('app').factory('AudioFactory', function($document) {
+  var audio = $document[0].createElement('audio');
+  return audio;
+});
+
