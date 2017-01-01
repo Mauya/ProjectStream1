ec07d778c01b280d41801ad950d101606cdb925e
Mauya <smkadungure@live.co.uk>
2017-01-01 16:12:39 +0000

    updating index file

--- js/controllers/MusicController.js
+++ js/controllers/MusicController.js
@@ -1,8 +1,9 @@
 angular.module('RouteControllers', [])
 app.controller('MusicController', ['$scope', 'audiofactory', function($scope, audiofactory) {
+	$scope.musicTitle='The Crew Music' //can list as much of their music as they want.
     $scope.name = audioService.audioPlayer;
     $scope.update = function(){
       $scope.name.isPaused;
       $scope.audioList=Audio;
     }
-}])
+}]);
